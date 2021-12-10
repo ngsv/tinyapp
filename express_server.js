d77fda647ec607084cfca8398c5e91cfbd33e46c
Steven Ngov <gl.steven.ngov@gmail.com>
2021-12-10 23:13:38 +0000

    Add another page for a URL submission form

--- express_server.js
+++ express_server.js
@@ -3,10 +3,15 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = () => {
+  return Math.random().toString(36).slice(2, 8);
 };
 
 app.get("/", (req, res) => {
@@ -16,6 +21,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
